9a29796d0f2fdffc93318412cb97b7dae46c885b
Ian Wilkey <iwilkey@mail.bradley.edu>
2022-03-27 22:32:15 -0500

    Jacobian updated to 1.1.3

---
+++ jacobian.cpp
@@ -0,0 +1,283 @@
+/**
+ * JacobianOS implementation file.
+ * 
+ * @author Ian Wilkey (iwilkey)
+ * @since 1.0.0
+ */
+
+#include "jacobian.h"
+#include <wiringPi.h>
+using namespace jacobian;
+
+/*******************
+General utilities
+/*******************/
+
+// General logging. These methods print a simple message to the terminal during runtime.
+template <typename T>
+void jacobian::log(const string tag, const T obj) {
+	cout << "JacobianOS [Debug Log] @ t = " << clock() 
+		<< ": " << tag << ": " << obj << endl;
+	return;
+}
+template <typename T>
+void jacobian::log(const T obj) {
+	cout << "JacobianOS [Debug Log] @ t = " << clock() 
+		<< ": " << obj << endl;
+	return;
+}
+
+/**
+ * This function translates time in seconds to duty cycle with defined frequency.
+ *
+ * @params
+ * 	int frequency: The frequency of the signal.
+ * 	float time: The time, in seconds, to be translated.
+ * @return the time as a percentage of period. 
+ */
+float jacobian::timeToDutyCycle(int frequency, float time) {
+	if(time <= 0.0f) return 0.0f;
+	float period = 1 / (double)frequency;
+	if(time >= period) return 100.0f;
+	return (time / period) * 100.0f;
+}
+
+/**
+ * Shift the decimal point of si by shift digits.
+ * 
+ * @params
+ * 	float si: The original number, preferred in it's deka state.
+ * 	int shift: The amount to shift the decimal.
+ * @return the shifted number.
+ */
+float jacobian::radixShift(float si, int shift) {
+	si *= pow(10, shift);
+	return si;
+}
+	
+/**
+ * Simple delay using system clock.
+ * 
+ * @params
+ * 	double s: The time to wait, in seconds.
+ * @return void
+ */
+void jacobian::waitForSeconds(double s) {
+	clock_t start = clock();
+	while(true) {
+		if((clock() - start) / (double)CLOCKS_PER_SEC > s)
+			break;
+	}	
+	return;
+}
+
+/*******************
+Controller object
+/*******************/
+
+// Controller constructor.
+Controller::Controller(string name) {
+	if(!init()) {
+		log("FATAL", "JacobianOS has encountered an error. See log.txt");
+		exit(-1);
+	}
+	setName(name);
+	setState(true);
+	log("Success", "JacobianOS successfully initialized.");
+}
+
+/**
+ * This function is called automatically when a new Controller is constructed.
+ * Its main purpose is to initiate wiringPI (selected GPIO library).
+ */
+bool Controller::init(void) {
+	if (wiringPiSetup() != 0) 
+		return false;
+	return true;
+}
+
+/**
+ * This function MUST be called after every JacobianOS program. It resets the state of the
+ * configured GPIO pins to a neutral state. This is to avoid any electrical discharge
+ * or damage of componets after use.
+ */
+void Controller::kill(void) {
+	for(pair<string, int> pin : pinout) {
+		setPin(pin.first, 0);
+		setPinMode(pin.first, INPUT);
+	}
+	log("Success", "Controller terminated. It is now safe to touch the electronic components.");
+	return;
+}
+
+// Return if the controller is currently overriden by manual controller.
+bool Controller::isOverridden(void) {
+	return this->overriden;
+}
+
+// Set the state of the Controller override.
+void Controller::Override(bool verdict) {
+	this->overriden = verdict;
+	if(verdict) log("Success", "The Controller is no longer in control of the RC car.")
+	else log("Success", "The Controller is now in direct control of the RC car.");
+	return;
+}
+
+// Return the distinct name of the Controller.
+string Controller::getName(void) {
+	return this->name;
+}
+
+// Set the name of the Controller.
+void Controller::setName(string name) {
+	this->name = name;
+	return;
+}
+
+// Set whether or not the Controller is on or off.
+void Controller::setState(bool state) {
+	this->running = state;
+}
+
+// Is the controller on or off?
+bool Controller::isRunning(void) {
+	return this->running;
+}
+
+/**
+ * This function will return the pin ID of the current active pinouts by name.
+ * 
+ * @params
+ * 	string pinName: The name of the pin you are searching for.
+ * @return the integer pin ID from name if it exists, otherwise -1.
+ */
+int Controller::returnPinFromName(string pinName) {
+	for(int i = 0; i < this->pinout.size(); i++) {
+		if(this->pinout[i].first == pinName)
+			return this->pinout[i].second;
+	}
+	log("Error", "No pin was found with this name: " + pinName);
+	return -1;
+}
+
+/**
+ * This function will set the pin mode (see WiringPI pinMode for specfic modes) of the pin by name, if it exists.
+ * 
+ * @params
+ * 	string pinName: The name of the pin you are setting the mode for.
+ * 	int mode: The mode you would like to set.
+ */
+void Controller::setPinMode(string pinName, int mode) {
+	int pin = returnPinFromName(pinName);
+	if(pin == -1) return;
+	pinMode(pin, mode);
+	return; 
+}
+
+/**
+ * This function will set the pin pull up resistor (see WiringPi pud for specific states) of the pin by name, if it exists.
+ * 
+ * @params
+ * 	string pinName: The name of the pin you are setting the pull up for.
+ * 	int pud: The pull up mode you are setting it to.
+ */
+void Controller::setPinPud(string pinName, int pud) {
+	int pin = returnPinFromName(pinName);
+	if(pin == -1) return;
+	pullUpDnControl(pin, pud);
+	return;
+}
+
+/**
+ * This function will read the digital value of the current pin by name, if it exists.
+ * 
+ * @params
+ * 	string pinName: The name of the pin.
+ * @return the value of the pin.
+ */
+int Controller::readPin(string pinName) {
+	int pin = returnPinFromName(pinName);
+	if(pin == -1) return -1;
+	return digitalRead(pin);
+}
+
+/**
+ * This function will set the digital value of the pin by name, if it exists.
+ * 
+ * @params
+ * 	string pinName: The name of the pin.
+ * 	int value: The digital value.
+ */
+void Controller::setPin(string pinName, int value) {
+	int pin = returnPinFromName(pinName);
+	if(pin == -1) return;
+	digitalWrite(pin, value);
+	return;
+}
+
+/**
+ * This function will add and configure a new pinout on the controller.
+ * You must create a pin with this function in order to edit properties of the pin.
+ * 
+ * @params
+ * 	int id: The ID (location) of the pin (use $ gpio readall to find correct ID).
+ * 	string name: The name of the pin you can use as a unique reference.
+ * 	int mode: The default mode of the pin.
+ * 	int pud: The default pull resistor mode of the pin.
+ */
+void Controller::configurePin(int id, string pinName, int mode, int pud) {
+	pair<string, int> pin = make_pair(pinName, id);
+	this->pinout.push_back(pin);
+	setPinMode(pinName, mode);
+	setPinPud(pinName, pud);
+	log("Success", "A new pin has been configured! Pin ID: " 
+		+ to_string(id) + ", pinName: " + pinName + ".");
+	return;
+}
+	
+/*******************
+Pulse Width Modulation generator
+/*******************/
+
+// PWM constructor.
+PWM::PWM(int freq, double duty) {
+	this->frequency = freq;
+	this->dutyCycle = duty;
+	precisionPeriod = (1 / (float)frequency) * (PRECISION);
+}
+
+/**
+ * Set the duty cycle of the PWM signal.
+ * 
+ * @params
+ * 	double duty: The duty cycle [0.1% - 100%].
+ */
+void PWM::setDutyCycle(double duty) {
+	duty = (duty > 100.0f) ? 100.0f : duty;
+	duty = (duty <= 0.0f) ? 0.1f : duty;
+	this->dutyCycle = duty;
+}
+
+/**
+ * This method must be called every undelayed loop cycle in PI main
+ * loop while controller is active. This will update the state of the
+ * PWM as time moves.
+ */
+void PWM::tick(void) {
+	last = now;
+	now = clock();
+	delta += (now - last);
+
+	precisionSince = delta / (CLOCKS_PER_SEC / PRECISION);
+	if(precisionSince <= (precisionPeriod * (this->dutyCycle / 100.0f))) {
+		on = true;
+		return;
+	} else if (precisionSince >= precisionPeriod)
+		delta = 0;
+	on = false;
+}
+
+// Evaluate the current state of the PWM signal: logic HIGH or LOW.
+bool PWM::eval(void) {
+	return this->on;
+}
---
+++ jacobian.h
@@ -0,0 +1,144 @@
+/**
+ * Jacobian is an abstraction layer for configuing and utilizing a Raspberry PI's
+ * GPIO pinouts. 
+ * 
+ * This software is implemented at the lowest level above the hardware of
+ * the Bradley IEEE self-driving RC car. In that context, it serves to decompose high
+ * level vector input into two PWM signals and generate them using the PI system clock as well
+ * as deliver the logic HIGH and LOW out of the GPIO pins.
+ * 
+ * @author Ian Wilkey (iwilkey)
+ * @since 1.0.0
+ */
+
+#include <iostream>
+#include <vector>
+#include <math.h>
+#include <utility>
+#include <time.h>
+#include <thread>
+using namespace std;
+
+/**
+* The Jacobian namespace encapsulates three main deliniations of tools: general utilities, 
+* Pulse Width Modulation generator, and the Controller object.
+* 
+* @since 1.0.0
+*/
+namespace jacobian {
+	
+	/*******************
+	General utilities
+	/*******************/
+
+	/**
+	 * This set of constants, used in conjuction with the radixShift() utility,
+	 * can move the decimal of an SI number in base 10 to redefine it in the proper
+	 * context.
+	 * (ex: radixShift(1.2, MILLI) = 0.0012 seconds, or 1.2 milliseconds)
+	 *
+	 * @since 1.1.3
+	 */
+	enum RADIXSHIFT {
+		PICO = -12,
+		NANO = -9,
+		MICRO = -6,
+		MILLI = -3,
+		CENTI = -2,
+		DECI = -1,
+		HECTO = 2,
+		KILO = 3,
+		MEGA = 6,
+		GIGA = 9,
+		TERA = 12
+	};
+
+	float radixShift(float, int);
+
+	template <typename T>
+		void log(string, T); // With tag.
+	template <typename T>
+		void log(T); // Without tag.
+
+	float timeToDutyCycle(int, float);
+
+	void waitForSeconds(double);
+	
+	/*******************
+	Pulse Width Modulation generator
+	/*******************/
+
+	/**
+	 * This object creates a Pulse Width Modulation signal at specified frequency
+	 * and duty cycle. It operates soley by the change in the internal PI clock, so the mathematics
+	 * are done with the hope that the ticks are not delayed.
+	 * 
+	 * @since 1.1.0
+	 */
+	class PWM {
+		private:
+			// Data members.
+			bool on; // Is the PWM currently producing a logic HIGH or LOW?
+			int frequency; // (hz)
+			double dutyCycle; // (%)
+
+			// Internal clock state.
+			clock_t now = clock(), 
+				last, delta = 0, precisionSince,
+				precisionPeriod;
+
+		public:
+			const int PRECISION = pow(10, (float)MEGA); // The amount of decimal precision of the PWM clock.
+			
+			PWM(int, double);
+			void setDutyCycle(double);
+			void tick(void);
+			bool eval(void);
+	};
+	
+	/*******************
+	Controller object
+	/*******************/
+
+	/**
+	 *	The Jacobian controller class defines a controller with software configured GPIO
+	 * outputs or inputs. A controller is defined by name, and it's pins by ID.
+	 * It has many utility functions that are described in full above the .cpp implmentation
+	 * function headers.
+	 * 
+	 * @since 1.0.0
+	 */
+	class Controller {
+		private:
+			// Data members.
+			bool running = false, // Is the controller currently running?
+				overriden = false; // Is the controller being overriden by manual control?
+			string name; // Name of distinct controller.
+			vector< pair<string, int> > pinout; // Map of the configured GPIO pins during session.
+
+			bool init(void); // To solidify the configured GPIO pins.
+			
+		public:
+			Controller(string name);
+			
+			// Pin utilities.
+			int returnPinFromName(string);
+			void configurePin(int, string, int, int);
+			void setPinMode(string, int);
+			void setPinPud(string, int);
+			int readPin(string);
+			void setPin(string, int);
+
+			// Controller state.
+			bool isRunning(void);
+			void setState(bool);
+			bool isOverridden(void);
+			void Override(bool);
+
+			string getName(void);
+			void setName(string);
+
+			void kill(void); // MUST be called when ending program.
+	};
+
+}
